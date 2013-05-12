1dd86c3410476ee9a144a740130c4c6e33631717
Anthony McCormick <=>
2013-05-12 21:44:57 +0100

    Adding tests for multiple instances of create

--- test/mockgoose.spec.js
+++ test/mockgoose.spec.js
@@ -4,21 +4,26 @@ describe('Mockgoose Tests', function () {
     var mockgoose = require('../Mockgoose');
     var mongoose = require('mongoose');
     mockgoose(mongoose);
-    mongoose.connect('mongodb://localhost:3001/TestingDB');
+    mongoose.createConnection('mongodb://localhost:3001/TestingDB');
     var TestModel = require('./TestModel')(mongoose);
 
-    beforeEach(function () {
-        TestModel.create({email: 'valid@valid.com', password: 'password'}, function(err, model){});
+    beforeEach(function (done) {
+        TestModel.create({email: 'valid@valid.com', password: 'password'}, function (err, model) {
+            expect(model).toBeTruthy();
+            done(err);
+        });
     });
 
-    afterEach(function () {
+    afterEach(function (done) {
         //Reset the database after every test.
         mockgoose.reset();
+        done();
     });
 
     it('should be able to require mockgoose', function () {
         expect(mockgoose).toBeTruthy();
     });
+
     it('should be able to create a test model', function (done) {
         TestModel.create({email: 'email@email.com', password: 'supersecret'}, function (err, model) {
             expect(err).toBeFalsy();
@@ -37,12 +42,39 @@ describe('Mockgoose Tests', function () {
 
     });
 
-    it('should be able to call custom save pre', function () {
-        expect(false).toBeTruthy();
+    it('should be able to call custom save pre', function (done) {
+        TestModel.create({email: 'newemail@valid.com', password: 'password'}, function (err, model) {
+            //Custom pre save should encrypt the users password.
+            expect(model.password).not.toBe('password');
+            model.validPassword('password', function (err, success) {
+                expect(success).toBeTruthy();
+                expect(err).toBeFalsy();
+                done(err);
+            });
+
+        });
     });
 
-    it('should be able to find an item by id', function () {
+    it('should be able to create multiple items in one go', function (done) {
+        TestModel.create({email: 'one@one.com', password: 'password'},
+            {email: 'two@two.com', password: 'password'}, function (err, one, two) {
+                expect(err).toBeFalsy();
+                expect(one).toBeTruthy();
+                expect(two).toBeTruthy();
+                done(err);
+            });
+    });
 
+    it('should be able to find an item by id', function (done) {
+        TestModel.create({email: 'one@one.com', password: 'password'},
+            {email: 'two@two.com', password: 'password'}, function (err, one, two) {
+                expect(err).toBeFalsy();
+                TestModel.findById(two._id, function (err, model) {
+                    expect(err).toBeFalsy();
+                    expect(model._id.toString()).toBe(two._id.toString());
+                    done(err);
+                });
+            });
     });
 
 });
