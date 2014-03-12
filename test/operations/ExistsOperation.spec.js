d925558ae62698ab122a045dc62e9bee2ab37501
Anthony McCormick <anthony.mccormick@gmail.com>
2014-03-12 00:16:16 +0000

    Adding failing tests for $nin operation

---
+++ test/operations/ExistsOperation.spec.js
@@ -0,0 +1,184 @@
+describe('Mockgoose $exists Tests', function () {
+    'use strict';
+
+    var mockgoose = require('./../../Mockgoose');
+    var Mongoose = require('mongoose').Mongoose;
+    var mongoose = new Mongoose();
+    mockgoose(mongoose);
+    mongoose.connect('mongodb://localhost/TestingDB');
+
+    var Schema = new mongoose.Schema({
+        name: String,
+        email: String
+    });
+    var Model = mongoose.model('AllTests', Schema);
+
+    beforeEach(function (done) {
+        mockgoose.reset();
+        Model.create({
+                name: 'abc'
+            },
+            {
+                name: 'cde',
+                user: {
+                    email: 'test@test.com'
+                }
+            },
+            {
+                name: 'fgh',
+                email: 'test2@test.com'
+            }, function (err) {
+                done(err);
+            });
+    });
+
+    afterEach(function (done) {
+        //Reset the database after every test.
+        mockgoose.reset();
+        done();
+    });
+
+    describe('$exists Tests', function () {
+
+        it('Be able to match only one item with truthful $exists', function (done) {
+            Model.find({
+                email: { $exists: true }
+            }).exec().then(function (results) {
+                    expect(results).toBeDefined();
+                    expect(results.length).toBe(1);
+                    done();
+                }, done);
+        });
+
+        it('Be able to match only one item with deep truthful $exists', function (done) {
+            Model.find({
+                user: { email: { $exists: true } }
+            }).exec().then(function (results) {
+                    expect(results).toBeDefined();
+                    expect(results.length).toBe(1);
+                    done();
+                }, done);
+        });
+
+        it('Be able to match two items with false $exists', function (done) {
+            Model.find({
+                email: { $exists: false }
+            }).exec().then(function (results) {
+                    expect(results).toBeDefined();
+                    expect(results.length).toBe(2);
+                    done();
+                }, done);
+        });
+    });
+
+    describe('$exists mongodb examples', function () {
+        var Records = mongoose.model('Records', new mongoose.Schema({
+            order:Number,
+            a: Number,
+            b: Number,
+            c: Number
+        }));
+
+        beforeEach(function (done) {
+            Records.create(
+                { order: 0, a: 5, b: 5, c: null },
+                { order: 1, a: 3, b: null, c: 8 },
+                { order: 2, a: null, b: 3, c: 9 },
+                { order: 3, a: 1, b: 2, c: 3 },
+                { order: 4, a: 2, c: 5 },
+                { order: 5, a: 3, b: 2 },
+                { order: 6, a: 4 },
+                { order: 7, b: 2, c: 4 },
+                { order: 8, b: 2 },
+                { order: 9, c: 6 }, done);
+        });
+
+        it('a exists', function (done) {
+            Records.find({ a: { $exists: true } }).exec().then(function (results) {
+                expect(results.length).toBe(7);
+                var expected = [
+                    { order: 0, a: 5, b: 5, c: null },
+                    { order: 1, a: 3, b: null, c: 8 },
+                    { order: 2, a: null, b: 3, c: 9 },
+                    { order: 3, a: 1, b: 2, c: 3 },
+                    { order: 4, a: 2, c: 5 },
+                    { order: 5, a: 3, b: 2 },
+                    { order: 6, a: 4 }
+                ];
+                assertResults(results, expected);
+                done();
+            });
+        });
+
+        it('b does not exists', function (done) {
+            Records.find({ b: { $exists: false } }).exec().then(function (results) {
+                expect(results.length).toBe(3);
+                var expected = [
+                    { order: 4, a: 2, c: 5 },
+                    { order: 6, a: 4 },
+                    { order: 9, c: 6 }
+                ];
+                assertResults(results, expected);
+                done();
+            });
+        });
+
+        it('c does not exists', function (done) {
+            Records.find({ c: { $exists: false } }).exec().then(function (results) {
+                expect(results.length).toBe(3);
+                var expected = [
+                    { order: 5, a: 3, b: 2 },
+                    { order: 6, a: 4 },
+                    { order: 8, b: 2 }
+                ];
+                assertResults(results, expected);
+                done();
+            });
+        });
+
+
+        //-------------------------------------------------------------------------
+        //
+        // Private Methods
+        //
+        //-------------------------------------------------------------------------
+        function assertResults(results, expected) {
+            expected = expected.sort(orderSort);
+            var resultSet = toResultSet(results)
+            for (var i = 0; i < expected.length; i++) {
+                expect(resultSet[i]).toEqual(expected[i]);
+            }
+        }
+
+        function orderSort(a, b) {
+            if (a.order > b.order) {
+                return 1;
+            }
+            if (a.order < b.order) {
+                return -1;
+            }
+            return 0;
+        }
+
+        function toResultSet(results) {
+            var items = [];
+
+            function extractValue(result, resultItem, value) {
+                if (result[value] || result[value] === null || result[value] === 0) {
+                    resultItem[value] = result[value];
+                }
+            }
+
+            results.forEach(function (result) {
+                var resultItem = {};
+                extractValue(result, resultItem, 'order');
+                extractValue(result, resultItem, 'a');
+                extractValue(result, resultItem, 'b');
+                extractValue(result, resultItem, 'c');
+                items.push(resultItem);
+            });
+            return items.sort(orderSort);;
+        }
+
+    });
+});
--- test/operations/comparison/NINOperation.spec.js
+++ test/operations/comparison/NINOperation.spec.js
@@ -4,59 +4,38 @@ ddescribe('Mockgoose $nin', function () {
     var mockgoose = require('../../../Mockgoose');
     var Mongoose = require('mongoose').Mongoose;
     var mongoose = new Mongoose();
-//	mockgoose(mongoose);
-    mongoose.createConnection('mongodb://localhost/TestingDB2');
-
-    var StudentSchema = new mongoose.Schema({
-        _id: Number,
-        grades: [
-            {type: Number}
-        ]
-    });
-    var Student = mongoose.model('Students', StudentSchema);
-
-    beforeEach(function (done) {
-        mockgoose.reset();
-        Student.create({ '_id': 1, 'grades': [ 80, 85, 90 ] },
-            { '_id': 2, 'grades': [ 88, 90, 92 ] },
-            { '_id': 3, 'grades': [ 85, 100, 90 ] },
-            function (err) {
-                done(err);
-            });
-
-    });
+	mockgoose(mongoose);
+    mongoose.connect('mongodb://localhost/TestingDB');
 
     var Schema = new mongoose.Schema({
         name: String,
-        qty: [
-            {type: Number}
-        ]
+        qty: [Number]
     });
-    var Model = mongoose.model('TESTING', Schema);
+    var NinModel = mongoose.model('NinModel', Schema);
 
     beforeEach(function (done) {
         mockgoose.reset();
-        Model.create(
-//            {
-//                name: '5',
-//                qty: [5, 10, 15]
-//            },
-//            {
-//                name: '15',
-//                qty: [1, 15]
-//            },
-//            {
-//                name: '0',
-//                qty: [5, 8]
-//            },
-//            {
-//                name: '50',
-//                qty: [7, 8]
-//            },
+        NinModel.create(
+            {
+                name: '5',
+                qty: [5, 10, 15]
+            },
+            {
+                name: '15',
+                qty: [1, 15]
+            },
+            {
+                name: '0',
+                qty: [5, 8]
+            },
+            {
+                name: '50',
+                qty: [7, 8]
+            },
             {
                 name: '55',
                 qty: [7, 21]
-            }, function (err) {
+            }, function(err){
                 done(err);
             });
     });
@@ -70,14 +49,14 @@ ddescribe('Mockgoose $nin', function () {
     describe('', function () {
 
         it('Does not equal 5 or 15 in array', function (done) {
-            Model.find({ qty: { $nin: [ 5, 15 ] } }).exec().then(function (results) {
+            NinModel.find({ qty: { $nin: [ 5, 15 ] } }).exec().then(function (results) {
                 expect(results.length).toBe(2);
                 done();
             });
         });
 
         it('Does not equal 5 or 15 in field', function (done) {
-            Model.find({ name: { $nin: [ '5', '15' ] } }).exec().then(function (results) {
+            NinModel.find({ name: { $nin: [ '5', '15' ] } }).exec().then(function (results) {
                 expect(results.length).toBe(3);
                 done();
             });
