f0a1a3429d9e88f84648b80c50166fe745f3e6d7
Anthony McCormick <anthony.mccormick@gmail.com>
2014-05-03 21:43:41 +0100

    Added tests for  from mongodb website http://docs.mongodb.org/manual/reference/operator/update/positional/

--- test/operations/array/$update.spec.js
+++ test/operations/array/$update.spec.js
@@ -7,11 +7,58 @@ ddescribe('$(update)', function () {
     mockgoose(mongoose);
     mongoose.connect('mongodb://localhost/TestingDB');
 
+    describe('$(update)', function () {
+        var Schema = new mongoose.Schema({
+            _id: Number,
+            grades: [Number],
+            gradeDetails: [
+                {
+                    grade: Number,
+                    mean: Number,
+                    std: Number
+                }
+            ]
+        });
+
+        var Model = mongoose.model('UpdateModel', Schema);
+
+        beforeEach(function (done) {
+            mockgoose.reset();
+            Model.update({'_id': 1},
+                { 'grades': [ 80, 85, 90 ],
+                    'gradeDetails': [
+                        { grade: 80, mean: 75, std: 8 },
+                        { grade: 85, mean: 90, std: 5 },
+                        { grade: 90, mean: 85, std: 3 }
+                    ]
+                }, {upsert: true}, done);
+        });
+
+        it('Update first item', function (done) {
+            Model.update({ _id: 1, grades: 80 }, { $set: { 'grades.$': 82 } }).exec().then(function () {
+                Model.findById(1).exec().then(function (model) {
+                    expect(model.grades[0]).toBe(82);
+                    done();
+                });
+            });
+        });
+
+        it('Update nested fields', function (done) {
+            Model.update({ _id: 1, 'gradeDetails.grade': 85 }, { $set: { 'gradeDetails.$.std': 6 } }).exec().then(function () {
+                Model.findById(1).exec().then(function (model) {
+                    expect(model.gradeDetails[1].std).toBe(6);
+                    done();
+                });
+            });
+        });
+    });
+
     describe('Bugs', function () {
 
         describe('#46 https://github.com/mccormicka/Mockgoose/issues/46', function () {
 
             var Schema = new mongoose.Schema({
+                _id:Number,
                 p: String,
                 grps: [
                     {
@@ -29,19 +76,20 @@ ddescribe('$(update)', function () {
 
             beforeEach(function (done) {
                 mockgoose.reset();
-                Model.create({
-                    p: 'SomeId',
-                    grps: [
-                        {
-                            grpId: 'SomeGrpId',
-                            attrs: [
-                                {
-                                    attrId: 'SomeAttrId'
-                                }
-                            ]
-                        }
-                    ]
-                }).then(done);
+                Model.update({'_id': 1},
+                    {
+                        p: 'SomeId',
+                        grps: [
+                            {
+                                grpId: 'SomeGrpId',
+                                attrs: [
+                                    {
+                                        attrId: 'SomeAttrId'
+                                    }
+                                ]
+                            }
+                        ]
+                    }, {upsert: true}, done);
             });
 
             it('Not throw an error when using the update positional operator.', function (done) {
