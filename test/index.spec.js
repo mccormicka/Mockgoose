c8ecfbb2bec561293144a91aa3b8ba446c9820db
Anthony McCormick <anthony.mccormick@gmail.com>
2014-05-03 23:27:34 +0100

    Adding tests for #58

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,7 +5,7 @@ describe('Index Tests', function () {
     var mockgoose = require('../Mockgoose');
     var Mongoose = require('mongoose').Mongoose;
     var mongoose = new Mongoose();
-    mockgoose(mongoose);
+//    mockgoose(mongoose);
     var db = mongoose.connect('mongodb://localhost:27017/TestingDB');
 
     var collection;
@@ -15,34 +15,49 @@ describe('Index Tests', function () {
         done();
     });
 
-    describe('SHOULD', function () {
+    describe('Setup Indexes', function () {
+        var indexSchema = {
+            expire: {
+                type: Date,
+                expires: 1000,
+                'default': Date.now
+            }
+        };
+        var IndexModel = db.model('indexmodel', indexSchema);
 
-        describe('Setup Indexes', function () {
-            var indexSchema = {
-                expire: {
-                    type: Date,
-                    expires: 1000,
-                    'default': Date.now
-                }
-            };
-            var IndexModel = db.model('indexmodel', indexSchema);
-
-            beforeEach(function(done){
-                IndexModel.create({}, function(err, model){
-                    expect(err).toBeFalsy();
-                    expect(model).toBeTruthy();
-                    collection = model.collection;
-                    done();
-                });
-            });
-
-            it('Be able to retrieve indexes from a model', function (done) {
-                collection.getIndexes(function(err, indexes){
-                    expect(indexes).toEqual({ _id_ : [ [ '_id', 1 ] ], expire_1 : [ [ 'expire', 1 ] ] });
-                    done();
-                });
+        beforeEach(function (done) {
+            IndexModel.create({}, function (err, model) {
+                expect(err).toBeFalsy();
+                expect(model).toBeTruthy();
+                collection = model.collection;
+                done();
             });
         });
 
+        it('Be able to retrieve indexes from a model', function (done) {
+            collection.getIndexes(function (err, indexes) {
+                expect(indexes).toEqual({ _id_: [
+                    [ '_id', 1 ]
+                ], expire_1: [
+                    [ 'expire', 1 ]
+                ] });
+                done();
+            });
+        });
+    });
+
+    describe('Bugs', function () {
+
+        ddescribe('#58 https://github.com/mccormicka/Mockgoose/issues/58', function () {
+            it('Support duplicate validation on `index: { unique: true} }`', function (done) {
+                expect(function () {
+                    new mongoose.Schema({
+                        index: {unique: true}
+                    });
+                }).not.toThrow();
+                done();
+            });
+
+        });
     });
 });
