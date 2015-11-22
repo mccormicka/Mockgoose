481570e8d59309750d49d17701215ce1f24dfb03
Roman Jurkov <winfinit@gmail.com>
2015-11-21 23:32:16 -0500

    added simple test

--- test/index.js
+++ test/index.js
@@ -5,6 +5,9 @@ var Mongoose = require('mongoose').Mongoose;
 var mongoose = new Mongoose();
 
 var mockgoose = require('../Mockgoose');
+
+var Cat = mongoose.model('Cat', { name: String });
+
 mockgoose(mongoose);
 
 before(function(done) {
@@ -14,7 +17,21 @@ before(function(done) {
 });
 
 describe('User functions', function() {
-	it("should create a user foo", function(done) {
-            done();
+	it("should create a cat foo", function(done) {
+		Cat.create({name: "foo"}, function(err, cat) {
+            done(err);
+		});
+    });
+
+    it("should find cat foo", function(done) {
+    	Cat.findOne({name: "foo"}, function(err, cat) {
+    		done(err);
+    	});
+    });
+
+    it("should remove cat foo", function(done) {
+    	Cat.remove({name: "foo"}, function(err, cat) {
+    		done(err);
+    	});
     });
 });
