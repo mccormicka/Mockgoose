e74da523bb06f140fc6025819475c08f1e36fa93
Roman Jurkov <winfinit@gmail.com>
2016-01-12 21:50:02 -0500

    added removealllisteners

--- Mockgoose.js
+++ Mockgoose.js
@@ -75,6 +75,7 @@ module.exports = function(mongoose, db_opts) {
                 emitter.emit('mongodbStarted', db_opts);
             } else {
                 db_opts.port++;
+                mongod_emitter.removeAllListeners();
                 start_server(db_opts);
             }
         });
