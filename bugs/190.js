5d5cf3e656343f52624864a8e6fbff682c444370
Roman Jurkov <winfinit@gmail.com>
2016-02-16 21:54:13 -0500

    added license

---
+++ bugs/190.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const mockgoose = require('../mockgoose')
+
+mockgoose( mongoose )
+
+// Create the mongoose connection on init
+before(function(done) {
+    mongoose.connect('mongodb://example.com/TestingDB', function(err) {
+        done(err)
+    })
+});
+
+// Close the fake connection after all tests are done
+after(function(done) {
+    console.log('Closing') // Shows in console (always)
+    mongoose.connection.close(function() {
+        console.log('Closed') // Also. always shows in console
+        done()
+    })
+})
+
+
+describe('Foobar', function () {
+    describe('.createFoo', function () {
+        it( 'Creating foo with no data', function( done ) {
+            expect( null ).to.not.equal( null );
+            done();
+        })
+    });
+});
